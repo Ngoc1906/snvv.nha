a15e5816d4fdfc133b78c5ead6929537bc365c20
Ngoc1906 <tranngoc190620077@gmail.com>
2024-11-24 18:57:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy Birthday';
+const titleCard = 'Tặng Kphuongw';
+const contentCard = 'Chúc Kphuongw tuổi mới học giỏi dtwh xink đẹp có nhìu sức khỏe nè với đậu nguyên vọng 1 nghen';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'kp.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
